d83dd563ba3d5fb7a6f65ba8e26e26c489a98c8c
Juli4g1 <422009524@pcpuma.acatlan.unam.mx>
2023-04-24 15:41:13 -0500

    primer cambio

---
+++ facuamigas/ViewController cuestionario4.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController cuestionario4.swift
+//  facuamigas
+//
+//  Created by CEDAM04 on 24/04/23.
+//
+
+import UIKit
+
+class ViewController_cuestionario4: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
